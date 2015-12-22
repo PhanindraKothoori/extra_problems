e334dc9d4cd882a4194697d42ea12aa3a12efb0d
PhanindraKothoori <phani.kothoori@gmail.com>
2015-12-22 13:18:14 +0530

    insert_node_to_CLL.cpp

--- 2015_30_11/insert_node_to_CLL.cpp
+++ 2015_30_11/insert_node_to_CLL.cpp
@@ -8,44 +8,47 @@ struct node {
 
 
 void insert_node(struct node *head, int num, int pos){
-	if (pos == 0){//insert at head
+	if (head){
+		if (pos == 0){//insert at head
+			struct node *temp = head;
+			do{
+				temp = temp->next;
+			} while (temp->next != head->next);
+
+
+			struct node *newNode = (struct node*)malloc(sizeof(struct node));
+			newNode->num = num;
+			newNode->next = head->next;
+			head->next = newNode;
+			temp->next = newNode;
+		}
+
+		else{
+			int i = 0;
+			struct node *temp = head;
+			while (i < pos){
+				temp = temp->next;
+				i++;
+			}
+			struct node *newNode = (struct node*)malloc(sizeof(struct node));
+			newNode->num = num;
+			newNode->next = temp->next;
+			temp->next = newNode;
+		}
+	}
+}
+
+void insert_node(struct node* head, int num){//default insert at the end
+	if (head){
 		struct node *temp = head;
 		do{
 			temp = temp->next;
 		} while (temp->next != head->next);
-
-		
 		struct node *newNode = (struct node*)malloc(sizeof(struct node));
 		newNode->num = num;
-		newNode->next = head->next;
-		head->next = newNode;
+		newNode->next = temp->next;
 		temp->next = newNode;
-
-		return;
 	}
-
-
-	int i = 0;
-	struct node *temp = head;
-	while (i < pos){
-		temp=temp->next;
-		i++;
-	}
-	struct node *newNode = (struct node*)malloc(sizeof(struct node));
-	newNode->num = num;
-	newNode->next = temp->next;
-	temp->next = newNode;
-}
-
-void insert_node(struct node* head, int num){//default insert at the end
-	struct node *temp=head;
-	do{
-		temp = temp->next;
-	} while (temp->next != head->next);
-	struct node *newNode = (struct node*)malloc(sizeof(struct node));
-	newNode->num = num;
-	newNode->next = temp->next;
-	temp->next = newNode;
 }
 
 void myprint(struct node* head){
@@ -76,13 +79,14 @@ int main(){
 	start->next = start;
 	head->next = start;
 	Node = start;
-	int a[] = { 1, 2, 3, 4, 5, 6 }, i=0;
+	int a[] = { 1, 2, 3, 4, 5, 6 }, i = 0;
 	for (i = 1; i < 6; i++){
 		Node->next = create(a[i]);
 		Node = Node->next;
 	}
 	Node->next = start;
-	
+	//initial case
+	myprint(head);
 	//test cases
 	insert_node(head, 10, 3);
 	myprint(head);
